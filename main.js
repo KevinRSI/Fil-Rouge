5d4788c921297b0ae2011810818e971f5faa1859
KevinRSI <rousseau.kevin.simplon@gmail.com>
2020-12-14 17:20:11 +0100

    1st

---
+++ main.js
@@ -0,0 +1,12 @@
+
+    $(document).ready(function() {
+    $('.autoclose').on('hidden.bs.modal', function() {
+    var $this = $(this).find('iframe'),
+        tempSrc = $this.attr('src');
+    $this.attr('src', "");
+    $this.attr('src', tempSrc);
+});
+
+    
+});
+
