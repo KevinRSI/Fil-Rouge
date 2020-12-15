27d6019ad7fe81f6b64439cadb5bb72dc6f1c941
KevinRSI <rousseau.kevin.simplon@gmail.com>
2020-12-15 16:00:02 +0100

    fix js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-    $(document).ready(function() {
-    $('.autoclose').on('hidden.bs.modal', function() {
-    var $this = $(this).find('iframe'),
-        tempSrc = $this.attr('src');
-    $this.attr('src', "");
-    $this.attr('src', tempSrc);
+$(document).ready(function() {
+$('.autoclose').on('hidden.bs.modal', function() {
+var $this = $(this).find('iframe'),
+    tempSrc = $this.attr('src');
+$this.attr('src', "");
+$this.attr('src', tempSrc);
 });
 
     
